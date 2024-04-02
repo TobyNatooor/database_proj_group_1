c75c2a8920c0ebe5a5ef391c7902816c390ae23f
Marcus Lemser <s230985@dtu.dk>
2024-04-02 12:30:08 +0200

    Query 1# and 2# done

--- 1_02327DatabaseScript2_2024.sql
+++ 1_02327DatabaseScript2_2024.sql
@@ -1,6 +1,9 @@
 USE dkavisendb;
 
 -- Showing the most read article in each topic
+select Topic, articleTitle as Most_Read, max(nrofreaders) as readers
+from article
+group by Topic order by readers desc;
 
 -- Lists the top 10 journalists with most amount of total readers
 select FirstName, LastName, SUM(NrOfReaders) as readers
