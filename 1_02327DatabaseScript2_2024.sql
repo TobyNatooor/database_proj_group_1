59668e503dd610869c5e4e116908cd12d3db514b
Maximillian Bjørn Mortensen <s236167@dtu.dk>
2024-04-03 00:23:04 +0200

    writer and reporter

--- 1_02327DatabaseScript2_2024.sql
+++ 1_02327DatabaseScript2_2024.sql
@@ -1,27 +1,35 @@
 USE dkavisendb;
 
--- Showing the most read article in each topic
-select Topic, articleTitle as Most_Read, max(nrofreaders) as readers
-from article
-group by Topic order by readers desc;
-
 -- Lists the top 10 journalists with most amount of total readers
-select FirstName, LastName, SUM(NrOfReaders) as readers
-from Article natural join writer as w join journalist as j where j.CPR = w.writer
-group by writer order by readers desc
+select FirstName, LastName, SUM(NrOfReaders) as Readers
+from Article natural join Writer as w join Journalist as j where j.CPR = w.writer
+group by Writer order by Readers desc
 limit 10;
 
+-- Identify which journalists were both writers and reporters, having shot at least a photo that was used for a news article they wrote
+select w.ArticleTitle, j.FirstName, j.LastName
+from Writer w
+join IncludesPhoto ip on w.ArticleTitle = ip.ArticleTitle
+join Photo p on ip.PhotoTitle = p.PhotoTitle
+join Journalist j on p.Reporter = j.CPR
+where j.CPR = w.writer;
+
+-- Showing the most read article in each topic
+select Topic, ArticleTitle as Most_Read, max(NrOfReaders) as Readers
+from Article
+group by Topic order by Readers desc;
+
 -- Show reporters whose photos were never used more than once
-SELECT DISTINCT j.FirstName, j.LastName
-FROM journalist j
-JOIN Photo p ON j.CPR = p.Reporter
-LEFT JOIN IncludesPhoto ip ON p.PhotoTitle = ip.PhotoTitle
-GROUP BY j.CPR, j.FirstName
-HAVING COUNT(DISTINCT p.PhotoTitle) > 0
-AND COUNT(DISTINCT ip.ArticleTitle) < 2;
+select distinct j.FirstName, j.LastName
+from Journalist j
+join Photo p on j.CPR = p.Reporter
+left join IncludesPhoto ip on p.PhotoTitle = ip.PhotoTitle
+group by j.CPR, j.FirstName
+having COUNT(distinct p.PhotoTitle) > 0
+and COUNT(distinct ip.ArticleTitle) < 2;
 
 -- Identify which topics, overall, attracted less reads that the average
-select Topic
-from article
+select Topic, avg(NrOfReaders) AverageNrOfReadersForTopic
+from Article
 group by Topic
-having avg(nrOfReaders) < (select avg(nrOfReaders) from article);
+having avg(NrOfReaders) < (select avg(NrOfReaders) from Article);
